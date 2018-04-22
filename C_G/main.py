0d5f7cf7b0e22f58f71a359d4ddfcaec690b4d77
bjy_notebook <whddus6712@naver.com>
2018-04-22 16:43:00 +0900

    배경 움직임 추가

--- C_G/main.py
+++ C_G/main.py
@@ -3,9 +3,10 @@ import pygame
 WHITE = (255, 255, 255)
 pad_width = 600
 pad_heigth = 480
+background_width = 799
 
-def back(x,y):
-    global gamepad, background
+def back(background,x,y):
+    global gamepad
     gamepad.blit(background,(x,y))
 
 def player(x,y):
@@ -13,14 +14,15 @@ def player(x,y):
     gamepad.blit(chicken, (x,y))
 
 def runGame():
-    global gamepad, clock, chicken, background
+    global gamepad, clock, chicken, background1, background2
 
     x = pad_heigth *0.05
     y = pad_heigth * 0.8
     x_change = 0
     y_change = 0
 
-    background_x = 0
+    background1_x = 0
+    background2_x = background_width 
 
     crashed = False
     while not crashed:
@@ -45,10 +47,19 @@ def runGame():
         y += y_change
         x += x_change
 
-
-
         gamepad.fill(WHITE)
-        back(background_x, 0)
+
+        background1_x-=2
+        background2_x-=2
+
+        if background1_x == -background_width:
+            background1_x = background_width
+        if background2_x == -background_width:
+            background2_x = background_width
+
+
+        back(background1, background1_x, 0)
+        back(background2, background2_x, 0)
         player(x,y)
         pygame.display.update()
         clock.tick(30)
@@ -57,14 +68,14 @@ def runGame():
     quit()
 
 def initGame():
-    global gamepad, clock, chicken, background
+    global gamepad, clock, chicken, background1, background2
 
     pygame.init()
     gamepad = pygame.display.set_mode((pad_width, pad_heigth))
     pygame.display.set_caption('ChickenGun')
     chicken = pygame.image.load('resources/images/chicken.png')
-    background = pygame.image.load('resources/images/background.png')
-
+    background1 = pygame.image.load('resources/images/background.png')
+    background2 = background1.copy()
     clock = pygame.time.Clock()
     runGame()
 
